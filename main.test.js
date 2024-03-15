2bc7adee46458b42d75b3af8aac2e5a56f57507a
Gavin P <gavinapersad@gmail.com>
2024-03-15 10:32:40 +0000

    all boundary tests passing

--- main.test.js
+++ main.test.js
@@ -46,7 +46,7 @@ test("bare bones", function (){
     expect(actual).toBe(expected);
     });
 
-    // 1 lowest possible out test
+    // 1 lowest not possible out test
     test("1 should return No finish possible", function (){
     //Arrange
     const currentScore = 1;
@@ -57,3 +57,35 @@ test("bare bones", function (){
     expect(actual).toBe(expected);
     });
         
+   // 2 lowest possible out test
+   test("2 should return 2", function (){
+    //Arrange
+    const currentScore = 2;
+    const expected = 2;
+    //Act
+    const actual = canIFinish(currentScore);
+    //Assert
+    expect(actual).toBe(expected);
+    });
+
+    // 179 lowest not possible under 180 out test
+    test("179 should return No finish possible", function (){
+    //Arrange
+    const currentScore = 179;
+    const expected = "No finish possible";
+    //Act
+    const actual = canIFinish(currentScore);
+    //Assert
+    expect(actual).toBe(expected);
+    });
+
+    // 177 highest possible out test
+     test("177 should return 177", function (){
+    //Arrange
+    const currentScore = 177;
+    const expected = 177;
+    //Act
+    const actual = canIFinish(currentScore);
+    //Assert
+    expect(actual).toBe(expected);
+    });
